baa38875cb117adbc921c172e06e1bdf92cd116e
alain <alainbloch@gmail.com>
2009-12-14 01:01:31 -0800

    created global constant for the plugin directory. added mock class and helper methods back here.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,19 @@
+PLUGIN_DIR = File.dirname(__FILE__)
+
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
-require 'config_test'
+require PLUGIN_DIR + '/load_schema.rb'
+
+# Our Mock class that we use to test the sanitization on
+class Mock < ActiveRecord::Base
+  html_sanitizer :sanitize => [:mock_string, :mock_text]
+end
+
+def html_string
+  %(<b>This will not be bolded.</b>)
+end
+
+def non_html_string
+  %(This will not be bolded.)
+end
