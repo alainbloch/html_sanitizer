9468f897c3eb9bc115e1d0ea6877d9e96c475f8e
alain <alainbloch@gmail.com>
2009-12-14 00:23:02 -0800

    removed mock class and html/non-html string methods.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,16 +2,3 @@ require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
 require 'config_test'
-
-# Our Mock class that we use to test the sanitization on
-class Mock < ActiveRecord::Base
-  html_sanitizer :sanitize => [:mock_string, :mock_text]
-end
-
-def html_string
-  %(<b>This will not be bolded.</b>)
-end
-
-def non_html_string
-  %(This will be bolded.)
-end
