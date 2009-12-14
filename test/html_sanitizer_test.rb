3c2aaeedbc85eb89ddb896b656cf8f7cafc8afd9
alain <alainbloch@gmail.com>
2009-12-14 01:02:38 -0800

    revised to have the correct path to test_helper and to load_schema only during the test.

--- test/html_sanitizer_test.rb
+++ test/html_sanitizer_test.rb
@@ -1,30 +1,18 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/test_helper.rb' 
 
 class HtmlSanitizerTest < ActiveSupport::TestCase
-  
-  # Our Mock class that we use to test the sanitization on
-  class Mock < ActiveRecord::Base
-    html_sanitizer :sanitize => [:mock_string, :mock_text]
-  end
-
-  def html_string
-    %(<b>This will not be bolded.</b>)
-  end
-
-  def non_html_string
-    %(This will be bolded.)
-  end
+  load_schema
   
   test "has a Mock class initialized" do
     assert_nothing_raised("NameError"){ Mock.new }
   end
   
-  test "will sanitize HTML from a string attribute" do
+  test "should sanitize HTML from a string attribute" do
     mock = Mock.create(:mock_string => html_string)
     assert_equal mock.mock_string, non_html_string
   end
   
-  test "will sanitize HTML from a text attribute" do
+  test "should sanitize HTML from a text attribute" do
     mock = Mock.create(:mock_text => html_string)
     assert_equal mock.mock_text, non_html_string
   end
