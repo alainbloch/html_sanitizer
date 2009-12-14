2b706275ba32f63788268bc4bac67f2815a267b2
alain <alainbloch@gmail.com>
2009-12-14 01:03:13 -0800

    mock_text should be text column.

--- test/schema.rb
+++ test/schema.rb
@@ -2,7 +2,7 @@ ActiveRecord::Schema.define(:version => 0) do
   
   create_table :mocks, :force => true do |t|
     t.column :mock_string, :string
-    t.column :mock_text, :string
+    t.column :mock_text, :text
   end
 
 end
